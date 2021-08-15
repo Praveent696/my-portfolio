15bb4f674c3bea61246bfced5b7fbd81878ebdbe
Parveen Kumar <parveen.kumar2@globallogic.com>
2021-08-15 11:56:56 +0530

    Updates

---
+++ precache-manifest.65d5c5d490980ae467f34951517038a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1647cd539c7f19ada4d5f0e70e1b8b",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "62a512badee41fd540d9",
+    "url": "/my-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "7068bb998b988ec3dbb1",
+    "url": "/my-portfolio/static/css/main.8d190636.chunk.css"
+  },
+  {
+    "revision": "62a512badee41fd540d9",
+    "url": "/my-portfolio/static/js/2.1a01dfd4.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/my-portfolio/static/js/2.1a01dfd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7068bb998b988ec3dbb1",
+    "url": "/my-portfolio/static/js/main.f47a8456.chunk.js"
+  },
+  {
+    "revision": "0923b8545f0547572d59",
+    "url": "/my-portfolio/static/js/runtime-main.48f418c3.js"
+  }
+]);
