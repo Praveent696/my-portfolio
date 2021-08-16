7ac20dbe0bd4598892836f0c5f24842ca1cf6b56
Parveen Kumar <parveen.kumar2@globallogic.com>
2021-08-16 11:49:52 +0530

    Updates

--- precache-manifest.27ef49a020499bfb8d78ffcd7cb475f3.js
+++ precache-manifest.27ef49a020499bfb8d78ffcd7cb475f3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2f716beecb194d2a667cb1bee3a37d06",
+    "revision": "e5cebce2dbce2b7314d9f8aa509b014e",
     "url": "/my-portfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/my-portfolio/static/css/2.d64471e2.chunk.css"
   },
   {
-    "revision": "11e0e8458589cef25083",
+    "revision": "39780dc6eb0b6400e0e0",
     "url": "/my-portfolio/static/css/main.d0ec95ce.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/my-portfolio/static/js/2.1a01dfd4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "11e0e8458589cef25083",
-    "url": "/my-portfolio/static/js/main.5664e073.chunk.js"
+    "revision": "39780dc6eb0b6400e0e0",
+    "url": "/my-portfolio/static/js/main.95f4e630.chunk.js"
   },
   {
     "revision": "0923b8545f0547572d59",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.5725baf41f5cf3a7dc9e9850e6446452.js"
+  "/my-portfolio/precache-manifest.27ef49a020499bfb8d78ffcd7cb475f3.js"
 );
 
 self.addEventListener('message', (event) => {
