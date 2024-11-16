67432a4c1e802c826305f8813880a2bf5b8fd419
Parveen Kumar <parveen.kumar@buffercode.in>
2024-11-16 23:22:40 +0530

    Deploy React app to GitHub Pages 11

---
+++ precache-manifest.97bdf38438f01a08038fb4341b41d670.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b08d9e7d6e0a45024c7cd25df0dd57",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "db5a3146c66b55de01fb",
+    "url": "/my-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "3dfdacc1a64556a41505",
+    "url": "/my-portfolio/static/css/main.d0ec95ce.chunk.css"
+  },
+  {
+    "revision": "db5a3146c66b55de01fb",
+    "url": "/my-portfolio/static/js/2.40d2235b.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/my-portfolio/static/js/2.40d2235b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dfdacc1a64556a41505",
+    "url": "/my-portfolio/static/js/main.41cc4eff.chunk.js"
+  },
+  {
+    "revision": "0923b8545f0547572d59",
+    "url": "/my-portfolio/static/js/runtime-main.48f418c3.js"
+  }
+]);
